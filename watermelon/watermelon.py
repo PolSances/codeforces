98237b459b6cdf98d74473d7c3bc7f9cfdb1ba4b
Pol Sances Guirao <140385185+PolSances@users.noreply.github.com>
2024-07-31 18:00:07 +0200

    Add file via upload

---
+++ watermelon/watermelon.py
@@ -0,0 +1,21 @@
+def main():
+    x = get_weight()
+    even(x)
+
+def even(x):
+    div = x % 2
+    if div == 0:
+        print("YES")
+    else:
+        print("NO")
+
+def get_weight():
+    while True: 
+        x = int(input("How much does it weigh? "))
+        if 1 <= x <= 100:
+            break
+        else:
+            continue
+    return x
+
+main()
