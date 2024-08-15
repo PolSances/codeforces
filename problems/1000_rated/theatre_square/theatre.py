90147c488f42f96c465b54c36e8a3d765e31654a
PolSances <polsances@gmail.com>
2024-08-15 10:14:05 +0200

    Begining theatre.py

---
+++ problems/1000_rated/theatre_square/theatre.py
@@ -0,0 +1,16 @@
+def main():
+    function1()
+
+def function1():
+    sizes = input().split
+    n = int(sizes[0])
+    m = int(sizes[1])
+    a = int(sizes[2])
+    if a == n == m:
+        stone_numbers = 1
+    if n-a > a:
+        print()
+
+
+    print(stone_numbers)
+main()
