539cd6cca1d698f7d1d66f607def40357cf1ea9b
PolSances <polsances@gmail.com>
2024-08-16 09:20:16 +0200

    1200 probem

---
+++ problems/1200_rated/test_of_love/test_of_love.py
@@ -0,0 +1,16 @@
+def main():
+    function1()
+
+def function1():
+    times = int(input())
+    for _ in range(times):
+        variables = input().split()
+        n = int(variables[0])
+        m = int(variables[1])
+        k = int(variables[2])
+        river = input()
+        for i in range(0,len(river)-1):
+            input()
+    
+
+main()
