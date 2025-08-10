a7d03c5d423d0352d59d3791c8ac2f07250e4311
PolSances <polsances@gmail.com>
2025-08-10 17:58:18 +0200

    Contest Div3 done only solved problem 1

--- contests/c++/contest_div3_1042/A.cpp
+++ contests/c++/contest_div3_1042/A.cpp
@@ -2,10 +2,61 @@
 
 using namespace std;
 
+
+int trobarAmesgranqueB(int numbers1[], int numbers2[], int len){
+    for(int i=0; i < len; i++){
+        if(numbers1[i] > numbers2[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int trobarBmesgranqueA(int numbers1[], int numbers2[], int len){
+    for(int i=0; i < len; i++){
+        if(numbers1[i] < numbers2[i]){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
 int main(){
     int t; // Number of cases 
     cin >> t;
     for(int i = 0; i<t; i++){
-        
+        int n; // longitud de les arrays que entren
+        cin >> n;
+
+        int array1[n];
+        int array2[n];
+
+        for(int i = 0; i < n; i++){
+            cin >> array1[i];
+        }
+
+        for(int i = 0; i < n; i++){
+            cin >> array2[i];
+        }
+
+        int jumps = 1;
+
+       while (true) {
+            int idxA = trobarAmesgranqueB(array1, array2, n);
+            if (idxA == -1) {
+                break; 
+            }
+            array1[idxA] -= 1;
+
+            int idxB = trobarBmesgranqueA(array1, array2, n);
+            if (idxB != -1) {
+                array1[idxB] += 1;
+            }
+            jumps++;
+        }
+        cout << jumps << endl;
     }
 }
+
+
--- contests/c++/contest_div3_1042/B.cpp
+++ contests/c++/contest_div3_1042/B.cpp
@@ -2,10 +2,38 @@
 
 using namespace std;
 
+
+void printArray(int array[], int length){
+    for(int i = 0; i < length; i++){
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(){
     int t; // Number of cases 
     cin >> t;
     for(int i = 0; i<t; i++){
-        
+        int n;
+        cin >> n;
+
+        int array[n];
+
+        if(n == 2){
+            array[0] = -1;
+            array[1] = 2;
+            printArray(array, n);
+            continue;
+        }
+
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0){
+                array[i] = -1;
+            }
+            else{
+                array[i] = 3;
+            }
+        }
+        printArray(array, n);
     }
 }
